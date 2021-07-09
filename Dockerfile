a0f42af3695abae9ab194d8c7ba867a09f14b1ca
root <root@xavi.home>
2021-07-09 17:28:27 +0200

    modified:   Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,14 @@ FROM jupyter/scipy-notebook
 
 RUN mkdir my-model
 ENV MODEL_DIR=/home/jovyan/my-model
-ENV MODEL_FILE_LDA=clf_lda.joblib
-ENV MODEL_FILE_NN=clf_nn.joblib
+ENV MODEL_FILE=svc_model.model
 
 COPY requirements.txt ./requirements.txt
-RUN pip install -r requirements.txt 
-
-COPY train.csv ./train.csv
-COPY test.json ./test.json
+RUN pip install -r requirements.txt
 
 COPY train.py ./train.py
 COPY api.py ./api.py
 
+EXPOSE 5000
 
 RUN python3 train.py
--- train.py
+++ train.py
@@ -1,61 +1,37 @@
-#!/usr/bin/python3
-# tain.py
-# Xavier Vasques 13/04/2021
-
-import platform; print(platform.platform())
-import sys; print("Python", sys.version)
-import numpy; print("NumPy", numpy.__version__)
-import scipy; print("SciPy", scipy.__version__)
+# Deploy ML with OpenShift on IBM Cloud
+# train.py
+# Xavier Vasques 03/06/2021
 
 import os
-import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neural_network import MLPClassifier
-import pandas as pd
-from joblib import dump
-from sklearn import preprocessing
+from sklearn import svm
+from joblib import dump, load
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
 def train():
-
     # Load directory paths for persisting model
 
     MODEL_DIR = os.environ["MODEL_DIR"]
-    MODEL_FILE_LDA = os.environ["MODEL_FILE_LDA"]
-    MODEL_FILE_NN = os.environ["MODEL_FILE_NN"]
-    MODEL_PATH_LDA = os.path.join(MODEL_DIR, MODEL_FILE_LDA)
-    MODEL_PATH_NN = os.path.join(MODEL_DIR, MODEL_FILE_NN)
-      
-    # Load, read and normalize training data
-    training = "./train.csv"
-    data_train = pd.read_csv(training)
-        
-    y_train = data_train['# Letter'].values
-    X_train = data_train.drop(data_train.loc[:, 'Line':'# Letter'].columns, axis = 1)
+    MODEL_FILE = os.environ["MODEL_FILE"]
+    MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
 
-    print("Shape of the training data")
-    print(X_train.shape)
-    print(y_train.shape)
-        
-    # Data normalization (0,1)
-    X_train = preprocessing.normalize(X_train, norm='l2')
-    
-    # Models training
-    
-    # Linear Discrimant Analysis (Default parameters)
-    clf_lda = LinearDiscriminantAnalysis()
-    clf_lda.fit(X_train, y_train)
-    
-    # Serialize model
-    from joblib import dump
-    dump(clf_lda, MODEL_PATH_LDA)
-        
-    # Neural Networks multi-layer perceptron (MLP) algorithm
-    clf_NN = MLPClassifier(solver='adam', activation='relu', alpha=0.0001, hidden_layer_sizes=(500,), random_state=0, max_iter=1000)
-    clf_NN.fit(X_train, y_train)
-       
-    # Serialize model
-    from joblib import dump, load
-    dump(clf_NN, MODEL_PATH_NN)
-        
+    # Load and split the data
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,shuffle=False)
+
+    # Print the data
+    print(X_train)
+    print(X_test)
+
+    # Train the model
+    clf = svm.SVC()
+    clf.fit(X_train, y_train)
+
+    print ("svm.SVC Model finished training")
+
+    # Save the trained model for online inference
+    dump(clf, MODEL_PATH)
+
 if __name__ == '__main__':
     train()
